78da97a30c22b5373e04b69601bab65a73c7b9e7
rubgithub <rubensatual@hotmail.com>
2024-05-24 19:40:41 -0400

    Update wizterm.lua

--- wizterm/wizterm/wizterm.lua
+++ wizterm/wizterm/wizterm.lua
@@ -31,12 +31,16 @@ clear -x
 
 # Draw welcome screen  
 $terminal = @"
-  _____                   _             _ 
- |_   _|                 (_)           | |
-   | | ___ _ __ _ __ ___  _ _ __   __ _| |
-   | |/ _ \ '__| '_ ` _ \| | '_ \ / _` | |
-   | |  __/ |  | | | | | | | | | | (_| | |
-   \_/\___|_|  |_| |_| |_|_|_| |_|\__,_|_|
-                                          
+fortune | cowsay
+ ____________________________________
+/ Try to relax and enjoy the crisis. \
+|                                    |
+\ -- Ashleigh Brilliant              /
+ ------------------------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||                                          
 "@
 Write-Host -ForegroundColor DarkYellow $terminal
